166b796edfceb423e84b43a918238e72953c994f
Ismael <iselpuro@hotmail.com>
2019-02-21 21:58:31 -0300

    Header fixes

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,7 @@ import React, { PureComponent } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import '../css/header.css';
-import '../css/material_icons.css';
+//import '../css/header.css';
 
 
 export default @connect(state => ({
@@ -56,55 +55,80 @@ class Header extends PureComponent {
     console.log('User: ' + JSON.stringify(loggedUser));
     return (
       <div>
-<nav className="navbar navbar-default navbar-expand-xl navbar-light">
-	<div className="navbar-header d-flex col">
-		<a className="navbar-brand" href="#"><i className="fa fa-cube"></i>Coopify</a>  		
-		<button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle navbar-toggler ml-auto">
-			<span className="navbar-toggler-icon"></span>
-			<span className="icon-bar"></span>
-			<span className="icon-bar"></span>
-			<span className="icon-bar"></span>
-		</button>
-	</div>
+<nav className="navbar" role="navigation" aria-label="main navigation">
+  <div className="navbar-brand">
+    <Link className="navbar-item" to="/">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+    </Link>
 
-	<div id="navbarCollapse" className="collapse navbar-collapse justify-content-start">
-		<ul className="nav navbar-nav">
-			<li className="nav-item active"><a href="#" className="nav-link">Home</a></li>
-			<li className="nav-item"><a href="#" className="nav-link">About</a></li>
-			<li className="nav-item dropdown">
-				<a data-toggle="dropdown" className="nav-link dropdown-toggle" href="#">Services <b className="caret"></b></a>
-				<ul className="dropdown-menu">					
-					<li><a href="#" className="dropdown-item">Web Design</a></li>
-					<li><a href="#" className="dropdown-item">Web Development</a></li>
-					<li><a href="#" className="dropdown-item">Graphic Design</a></li>
-					<li><a href="#" className="dropdown-item">Digital Marketing</a></li>
-				</ul>
-			</li>
-		</ul>
-		<form className="navbar-form form-inline">
-			<div className="input-group search-box">								
-				<input type="text" id="search" className="form-control" placeholder="Search by Name"/>
-				<span className="input-group-addon"><i className="material-icons"></i></span>
+    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" className="navbar-menu">
+    <div className="navbar-start">
+    <Link className="navbar-item" to="/home">Home</Link>
+
+      <a className="navbar-item">
+        Documentation
+      </a>
+
+      <div className="navbar-item has-dropdown is-hoverable">
+        <a className="navbar-link">
+          More
+        </a>
+
+        <div className="navbar-dropdown">
+          <a className="navbar-item">
+            About
+          </a>
+          <a className="navbar-item">
+            Jobs
+          </a>
+          <a className="navbar-item">
+            Contact
+          </a>
+          <hr className="navbar-divider"/>
+          <a className="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+    { !userDidLog ? (
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <div className="buttons">
+        <Link className="button is-primary" to='/signup'><strong>Sign up</strong></Link>
+        <Link className="button is-light" to="/login">Log in </Link>
+        </div>
+      </div>
+    </div>)
+    : 
+    (
+<div className="navbar-end">
+			<div className="navbar-item has-dropdown is-hoverable">
+				<a href="#" data-toggle="dropdown" className="navbar-link" aria-expanded="false">
+        
+           <img className="" src='https://www.tutorialrepublic.com/examples/images/avatar/2.jpg' alt="Avatar"/> 
+
+          {loggedUser.email} <b className="caret"></b>
+        </a>
+          
+				<div className="navbar-dropdown">
+					<a href="#" className="navbar-item"><i className="fa fa-user-o"></i> Profile</a>
+					<a href="#" className="navbar-item"><i className="fa fa-sliders"></i> Settings</a>
+					<hr className="navbar-divider"/>
+					<a href="#" className="navbar-item"><i className="material-icons"></i> Logout</a>
+				</div> 
 			</div>
-		</form>
-
-    {userDidLog ? (
-		<ul className="nav navbar-nav navbar-right ml-auto">
-			<li className="nav-item"><a href="#" className="nav-link notifications"><i className="fa fa-bell-o"></i><span className="badge">1</span></a></li>
-			<li className="nav-item"><a href="#" className="nav-link messages"><i className="fa fa-envelope-o"></i><span className="badge">10</span></a></li>
-			<li className="nav-item dropdown">
-				<a href="#" data-toggle="dropdown" className="nav-link dropdown-toggle user-action" aria-expanded="false"><img src='https://www.tutorialrepublic.com/examples/images/avatar/2.jpg' className="avatar" alt="Avatar"/> {loggedUser.email} <b className="caret"></b></a>
-				<ul className="dropdown-menu">
-					<li><a href="#" className="dropdown-item"><i className="fa fa-user-o"></i> Profile</a></li>
-					<li><a href="#" className="dropdown-item"><i className="fa fa-calendar-o"></i> Calendar</a></li>
-					<li><a href="#" className="dropdown-item"><i className="fa fa-sliders"></i> Settings</a></li>
-					<li className="divider dropdown-divider"></li>
-					<li><a href="#" className="dropdown-item"><i className="material-icons"></i> Logout</a></li>
-				</ul> 
-			</li>
-    </ul>)
-    : "" }
-	</div>
+</div>
+    )
+    }
+  </div>
 </nav>
       </div>
     );

18294e478004d35bbcea43e0aafa33fccfcb713e
Ismael <iselpuro@hotmail.com>
2019-05-05 18:47:38 -0300

    Changed url

--- src/client.js
+++ src/client.js
@@ -18,8 +18,8 @@ import './resources/css/style.css';
 
 const appInitialState = {};
 
-//global.API_URL = 'https://coopify-dev-backend.herokuapp.com'
-global.API_URL = 'http://192.168.0.104:3001' //TODO: CHANGE WHEN DEPLOYING TO DEV OR STAGE MAYBE A SWITCH AND HAVING MULTIPLE CONFIGS?
+global.API_URL = 'https://coopify-dev-backend.herokuapp.com'
+//global.API_URL = 'http://192.168.0.104:3001' //TODO: CHANGE WHEN DEPLOYING TO DEV OR STAGE MAYBE A SWITCH AND HAVING MULTIPLE CONFIGS?
 global.FB_APP_ID = '323887408477346'
 global.GOOGLE_APP_ID = '157449480210-9sr3ar7e5q2lm7emifvegj0r9ohr3vm6.apps.googleusercontent.com'
 //global.GOOGLE_APP_ID = '157449480210-dd88viejmm5ce2ia29j0cqpaul1ml5j4.apps.googleusercontent.com'

6878102856d807fe2ea2a1a3dc21d64f3523fdbb
unknown <agustico13@gmail.com>
2019-06-01 15:23:48 -0300

    some fixes

--- src/components/guest-layout.jsx
+++ src/components/guest-layout.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
 import Header from './header';
 import Footer from './footer';
 import 'react-toastify/dist/ReactToastify.css';

91ca5affa6d0e3920bbd4faac5af08190d7b65cd
Ismael <iselpuro@hotmail.com>
2019-06-30 20:31:49 -0300

    Service workers working

--- src/sw.js
+++ src/sw.js
@@ -1,16 +1,18 @@
-// eslint-disable-next-line
+
 const bgSyncPlugin = new self.workbox.backgroundSync.Plugin('myQueueName', {
   maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
 });
 
-self.workbox.routing.registerRoute(
-  /\/api\/v3\/run/,
-  self.workbox.strategies.networkOnly({
-    plugins: [bgSyncPlugin]
-  }),
-  'POST'
-);
+// self.workbox.routing.registerRoute(
+//   /\/api\/v3\/run/,
+//   self.workbox.strategies.networkOnly({
+//     plugins: [bgSyncPlugin]
+//   }),
+//   'POST'
+// );
 
-self.addEventListener('fetch', (event) => {
-    console.log(event)
-});
+self.workbox.routing.registerRoute(
+  /\/api\/offers/,
+  new self.workbox.strategies.NetworkFirst(),
+  'GET',
+);

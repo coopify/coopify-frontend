5b7f24e52abc34f6f750b677dc99976cc77f7c0a
Ismael <iselpuro@hotmail.com>
2019-07-01 22:09:27 -0300

    Refactor Reducers/Sagas reviews fix

--- src/sagas/user.js
+++ src/sagas/user.js
@@ -45,14 +45,6 @@ import {
   GET_GOALSUSER_FAILURE,
   SYNC_FB_SUCCESS,
   SYNC_FB_FAILURE,
-  GET_REVIEWS_SUCCESS,
-  GET_REVIEWS_FAILURE,
-  SEND_REVIEW_SUCCESS,
-  SEND_REVIEW_FAILURE,
-  CAN_REVIEW_SUCCESS,
-  CAN_REVIEW_FAILURE,
-  GET_USER_REVIEWS_FAILURE,
-  GET_USER_REVIEWS_SUCCESS,
   GET_USER_SUCCESS,
   GET_USER_FAILURE,
 } from '../reducers';
@@ -87,10 +79,6 @@ import {
   getGoalsUserAPICall,
   syncFBApiCall,
   sendRewardApiCall,
-  getReviewsAPICall,
-  sendReviewAPICall,
-  checkReviewAPICall,
-  getUserReviewsApiCall,
   getUserApiCall,
 } from '../api';
 
@@ -348,42 +336,6 @@ export function* syncFacebookAsync(payload) {
 
 export function* sendRewardAsync(payload) {
   yield sendRewardApiCall(payload.payload);
-}
-
-export function* getReviewsAsync(payload) {
-  const result = yield getReviewsAPICall(payload.payload);
-  if (result.status === 200) {
-    yield put({ type: GET_REVIEWS_SUCCESS, reviews: result.reviews });
-  } else {
-    yield put({ type: GET_REVIEWS_FAILURE, errorMessage: result.errorMessage });
-  }
-}
-
-export function* sendReviewAsync(payload) {
-  const result = yield sendReviewAPICall(payload.payload);
-  if (result.status === 200) {
-    yield put({ type: SEND_REVIEW_SUCCESS, review: result.review });
-  } else {
-    yield put({ type: SEND_REVIEW_FAILURE, errorMessage: result.errorMessage });
-  }
-}
-
-export function* canReviewAsync(payload) {
-  const result = yield checkReviewAPICall(payload.payload);
-  if (result.status === 200) {
-    yield put({ type: CAN_REVIEW_SUCCESS, canRate: result.canRate });
-  } else {
-    yield put({ type: CAN_REVIEW_FAILURE, errorMessage: result.errorMessage });
-  }
-}
-
-export function* getUserReviewsAsync(payload) {
-  const result = yield getUserReviewsApiCall(payload.payload);
-  if (result.status === 200) {
-    yield put({ type: GET_USER_REVIEWS_SUCCESS, reviews: result.reviews });
-  } else {
-    yield put({ type: GET_USER_REVIEWS_FAILURE, errorMessage: result.errorMessage });
-  }
 }
 
 export function* getUserAsync(payload) {

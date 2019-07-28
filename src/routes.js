219e27720c5ce5b622c436e4fd0d7ba2e5b827d9
Agustín Caétano <agustico13@gmail.com>
2019-07-28 19:33:10 -0300

    add icons 192 y 512 (#65)

--- src/routes.js
+++ src/routes.js
@@ -2,19 +2,31 @@ import GuestRoutes from './pages/guest';
 import AuthRoutes from './pages/auth';
 import SplashScreen from './pages/splash';
 import LandingScreen from './pages/landing';
+import PwaIcon192 from './resources/img/pwa-icon-192.png';
+import PwaIcon512 from './resources/img/pwa-icon-512.png';
 
 export default class Routes {
   // eslint-disable-next-line
   apply(routeHandler) {
     routeHandler.setPwaSchema({
-      name: "Coopify- Service Exchange Platform",
-      short_name: "Coopify",
-      lang: "en-US",
-      start_url: "/",
-      display: "standalone",
-      orientation: "portrait",
-      theme_color: "#007bff",
-      background_color: "#007bff"
+      name: 'Coopify- Service Exchange Platform',
+      short_name: 'Coopify',
+      lang: 'en-US',
+      start_url: '/',
+      display: 'standalone',
+      orientation: 'portrait',
+      theme_color: '#007bff',
+      background_color: '#007bff',
+      icons: [
+        {
+          src: PwaIcon192,
+          sizes: '192x192',
+        },
+        {
+          src: PwaIcon512,
+          sizes: '512x512',
+        },
+      ],
     });
     routeHandler.setDefaultSeoSchema({
       title: 'ReactPWA',
